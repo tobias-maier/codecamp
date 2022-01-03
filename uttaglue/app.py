3f2851b9c3dbf84113b30a70c15494067a9b4ee3
Tobias Maier <tobias.to.maier@deutschebahn.com>
2022-01-03 17:51:20 +0100

    Programm struktur

---
+++ uttaglue/app.py
@@ -0,0 +1,10 @@
+# Text, der die Regeln und das Spiel beshreibt
+
+# Einlesen des Bereichs, in dem sich die Zahl befindet
+
+# Einlesen der maximalen Anzahl von Versuchen
+
+# Schleife, in der Fred versucht die Zahl zu raten.
+# Spieler muss nun "g" oder "k" eingeben, je nach dem ob die Zahl größer oder kleiner ist
+
+# Ausgabe einer Beendigungsnachricht
